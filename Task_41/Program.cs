6aeb1ff3540f2dce36fe6e9ae791240477bda28b
RIP1982 <perovsky@list.ru>
2022-06-30 19:06:53 +0400

    Solution task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,75 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры числа через запятую. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+
+Console.WriteLine("Введите числа через запятую: ");
+int[] num = StrArray(Console.ReadLine());
+
+Console.Write("[");
+PrintArray(num);
+Console.Write("]");
+Console.WriteLine();
+
+int countPositiveNum = 0;
+
+for(int i = 0; i < num.Length; i++)
+{
+    if(num[i] > 0)
+    {
+        countPositiveNum++;
+    }
+}
+
+Console.WriteLine($"Количество положительных чисел = {countPositiveNum}");
+
+int [] StrArray(string output)
+{
+    int numCount = 0;
+
+    for (int i = 0; i < output.Length; i++)
+    {
+        if(output[i] == ',')
+        {
+            numCount++;
+        }
+    }
+
+    int[] num = new int[(numCount +1)];
+    int index = 0;
+
+    for (int i = 0; i < output.Length; i++)
+    {
+        string tempStr = ""; 
+        while(output[i] != ',')
+        {
+            if(i != output.Length - 1)
+            {
+                tempStr = tempStr + output[i];
+                i++;
+            }
+            else
+            {
+                tempStr = tempStr + output[i];
+                break;
+            }
+        }
+        num[index] = Convert.ToInt32(tempStr);
+        index++;
+    }
+    return num;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+
+
