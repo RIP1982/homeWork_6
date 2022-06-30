cfae1fb1ee9e3311a174258e9b5ca89afc0b1dc2
RIP1982 <perovsky@list.ru>
2022-06-30 19:10:57 +0400

    Solution task 41 with corrective

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -10,6 +10,10 @@ double k1 = Input("Введите значение k1: ");
 double b2 = Input("Введите значение b2: ");
 double k2 = Input("Введите значение k2: ");
 
+if(k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны!");
+}
 double x = (b2 - b1) / (k1 - k2);
 double y = k1 * x + b1;
 
